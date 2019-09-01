ed883d640346a49846df5d242397546edb8017e5
Joseph Shambrook <josephshambrook@googlemail.com>
2019-09-01 16:08:31 +0100

    Resolve shell errors

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,37 +1,21 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import { Link } from "gatsby"
 
 import happiness from "../../images/double-happiness.svg"
 import styles from "./header.module.css"
 
-const Header = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "double-happiness.png" }) {
-        childImageSharp {
-          fixed(height: 200, quality: 100, width: 200) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `)
-
-  return (
-    <header className={styles.header}>
-      <img className={styles.svg} src={happiness} />
-      {/* <Img fixed={data.placeholderImage.childImageSharp.fixed} /> */}
-      <h1>
-        <Link to="/" className={styles.link}>
-          Ling Zhu
-          <br />
-          &<br />
-          Edward Shambrook
-        </Link>
-      </h1>
-    </header>
-  )
-}
+const Header = () => (
+  <header className={styles.header}>
+    <img className={styles.svg} src={happiness} alt="双喜" />
+    <h1>
+      <Link to="/" className={styles.link}>
+        Ling Zhu
+        <br />
+        &<br />
+        Edward Shambrook
+      </Link>
+    </h1>
+  </header>
+)
 
 export default Header
--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/Layout/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
